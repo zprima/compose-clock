d08d3f5db5d8fc9a23e8598d871f94931dd584b3
Primož Žnidar <sg.prima@gmail.com>
2022-05-23 21:39:17 +0200

    Shortening the hour hand

--- app/src/main/java/com/example/composeclock/MainActivity.kt
+++ app/src/main/java/com/example/composeclock/MainActivity.kt
@@ -5,15 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -34,7 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeClockTheme {
-                val viewModel:ClockViewModel = viewModel()
+                val viewModel: ClockViewModel = viewModel()
 
                 ClockApp(viewModel)
             }
@@ -43,7 +42,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ClockApp(viewModel: ClockViewModel){
+fun ClockApp(viewModel: ClockViewModel) {
     val clockState by viewModel.clockState.collectAsState()
 
     Surface(Modifier.fillMaxSize()) {
@@ -56,18 +55,21 @@ fun ClockApp(viewModel: ClockViewModel){
 }
 
 @Composable
-fun ClockWatchFace(hours: Int, minutes: Int, seconds: Int){
-    val hoursAngle = (hours * radiansPerHour).toFloat() - (Math.PI/2).toFloat()
-    val minutesAngle = (minutes * radiansPerTick).toFloat() - (Math.PI/2).toFloat()
-    val secondsAngle = (seconds * radiansPerTick).toFloat() - (Math.PI/2).toFloat()
+fun ClockWatchFace(hours: Int, minutes: Int, seconds: Int) {
+    val hoursAngle =
+        remember(hours) { (hours * radiansPerHour).toFloat() - (Math.PI / 2).toFloat() }
+    val minutesAngle =
+        remember(minutes) { (minutes * radiansPerTick).toFloat() - (Math.PI / 2).toFloat() }
+    val secondsAngle =
+        remember(seconds) { (seconds * radiansPerTick).toFloat() - (Math.PI / 2).toFloat() }
 
     BoxWithConstraints(Modifier.fillMaxSize()) {
-        val maxWidthPx = with(LocalDensity.current){ maxWidth.toPx() }
-        val maxHeightPx = with(LocalDensity.current){ maxHeight.toPx() }
+        val maxWidthPx = with(LocalDensity.current) { maxWidth.toPx() }
+        val maxHeightPx = with(LocalDensity.current) { maxHeight.toPx() }
 
-        val center =  Offset(maxWidthPx / 2, maxHeightPx / 2)
+        val center = Offset(maxWidthPx / 2, maxHeightPx / 2)
 
-        Canvas(Modifier.fillMaxSize()){
+        Canvas(Modifier.fillMaxSize()) {
             drawCircle(
                 color = Color.Black,
                 center = center,
@@ -82,23 +84,27 @@ fun ClockWatchFace(hours: Int, minutes: Int, seconds: Int){
             )
 
             // hours
-            val hoursEnd = center + Offset(cos(hoursAngle), sin(hoursAngle)) * (maxWidthPx / 2)
+            val hoursEnd = center + Offset(cos(hoursAngle) * 0.8f, sin(hoursAngle) * 0.8f) * (maxWidthPx / 2)
             drawLine(
                 color = Color.Black,
                 start = center,
-                end = hoursEnd //Offset(center.x, center.y - (maxWidthPx / 2)),
+                end = hoursEnd, //Offset(center.x, center.y - (maxWidthPx / 2)),
+                strokeWidth = 5f
             )
 
             // minutes
-            val minutesEnd = center + Offset(cos(minutesAngle), sin(minutesAngle)) * (maxWidthPx / 2)
+            val minutesEnd =
+                center + Offset(cos(minutesAngle), sin(minutesAngle)) * (maxWidthPx / 2)
             drawLine(
                 color = Color.Blue,
                 start = center,
-                end = minutesEnd //Offset(center.x, center.y + (maxWidthPx / 2))
+                end = minutesEnd, //Offset(center.x, center.y + (maxWidthPx / 2))
+                strokeWidth = 3f
             )
 
             // seconds
-            val secondsEnd = center + Offset(cos(secondsAngle), sin(secondsAngle)) * (maxWidthPx / 2)
+            val secondsEnd =
+                center + Offset(cos(secondsAngle), sin(secondsAngle)) * (maxWidthPx / 2)
             drawLine(
                 color = Color.Red,
                 start = center,
@@ -106,7 +112,10 @@ fun ClockWatchFace(hours: Int, minutes: Int, seconds: Int){
             )
         }
 
-        Row(){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
             Text("$hours:$minutes:$seconds")
         }
     }
